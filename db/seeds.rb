c3cc85897d0f392e4a710e0de7d68a2f5ab39f78
MKP1992 <mohitkpaul92@gmail.com>
2024-06-06 21:44:06 -0400

    1. seed file added. 2. found a bug in view updated with create turbo stream.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Clear existing data
+Task.destroy_all
+
+# Create tasks
+3.times do |i|
+  Task.create!(
+    title: "Active Task #{i + 1}",
+    archived: false
+  )
+end
+
+3.times do |i|
+  Task.create!(
+    title: "Archived Task #{i + 1}",
+    archived: true
+  )
+end
+
+puts "Created #{Task.count} tasks"
